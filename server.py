ecbc189371dc0dd3ff35daacb8136e50a2709f91
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-11-11 08:48:20 -0300

    Adapted to 3 or more clients

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 from time import time, sleep
 from socket import *
 import argparse
+import os
 
 
 def main(args):
-    f = open('traffic.log', 'w').close()
     global_start = time() * 1000
     buffer_size = 50 * pow(2, 20)
+    client = 1
 
     with socket(AF_INET, SOCK_STREAM) as server_socket:
 
         server_socket.bind(('127.0.0.1', args.port))
-        server_socket.listen(2)
+        server_socket.listen()
 
         print('Server active.')
 
@@ -19,28 +20,36 @@ def main(args):
             conn, addr = server_socket.accept()
             print('Connected with {}'.format(addr))
 
-            with conn:
-                total_recv = 0
-                start = int(time() * 1000)
+            pid = os.fork()
 
-                while True:
-                    data = conn.recv(buffer_size)
+            if pid == 0:  # child
+                log_name = 'traffic{}.log'.format(client)
 
-                    if data:
-                        total_recv = total_recv + len(data)
-                    else:
-                        break
+                with conn:
+                    total_recv = 0
+                    start = int(time() * 1000)
 
-                    stop = int(time() * 1000)
-                    if (stop - start) > 1000:  # 1 second
-                        x = (stop - global_start) // 1000
-                        y = (total_recv * 8) / ((stop - start) / 1000)
+                    while True:
+                        data = conn.recv(buffer_size)
 
-                        total_recv = 0
-                        start = int(time() * 1000)
+                        if data:
+                            total_recv = total_recv + len(data)
+                        else:
+                            print("Connection ended {}".format(addr))
+                            exit()
 
-                        with open('traffic.log', 'a') as log:
-                            log.write("{},{}\n".format(x, y))
+                        stop = int(time() * 1000)
+                        if (stop - start) > 1000:  # 1 second
+                            x = (stop - global_start) // 1000
+                            y = (total_recv * 8) / ((stop - start) / 1000)
+
+                            total_recv = 0
+                            start = int(time() * 1000)
+
+                            with open(log_name, 'a') as log:
+                                log.write("{},{}\n".format(x, y))
+
+            client = client + 1
 
 
 if __name__ == "__main__":
--- traffic.py
+++ traffic.py
@@ -2,23 +2,38 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from matplotlib import style
 from pandas import read_csv
+from glob import glob
 
 style.use('ggplot')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
 
+color = ['r', 'b', 'g', 'y', 'c', 'm']
+
 
 def animate(frame):
-    log_data = read_csv('traffic.log', header=None, index_col=None)
-    # log_data.iloc[:, 1] = log_data.iloc[:, 1] / (8 * pow(2, 20))
-
     ax1.clear()
-    ax1.plot(log_data.iloc[:, 0], log_data.iloc[:, 1], '-o')
 
+    logs = glob('traffic*.log')
+    legends = []
+    plots = []
+    for log_file in logs:
+        i = int(log_file[7]) - 1
+        c = color[i]
+
+        log_data = read_csv(log_file, header=None, index_col=None)
+        # log_data.iloc[:, 1] = log_data.iloc[:, 1] / (8 * pow(2, 20))  # convert bps to mbps
+
+        plot, = ax1.plot(log_data.iloc[:, 0], log_data.iloc[:, 1], c)
+        plots.append(plot)
+
+        legends.append(log_file.replace('traffic', 'Cliente ')[:-4])
+
+    ax1.legend(plots, legends)
     ax1.set_xlabel("Tempo (s)")
     ax1.set_ylabel("bit/s")
 
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+ani = animation.FuncAnimation(fig, animate, interval=2000)
 plt.show()
